454fc05587e14310a7157d760a4ae2d7fef95217
arthur <gogoke1@naver.com>
2019-03-23 00:53:24 +0900

    setting +ㅅ+

---
+++ src/main/java/com/arthur/recommandlibrary/RecommandLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.recommandlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommandLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommandLibraryApplication.class, args);
+    }
+
+}
